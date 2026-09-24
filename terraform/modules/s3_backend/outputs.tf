f0d022f49a2cf66304c22cc216f4a03df1fdec09
jayssolutions <jayssolutions69@gmail.com>
2026-09-24 14:21:10 +0530

    Refactored to remove unused module

--- terraform/modules/s3_backend/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_id" {
-  value       = aws_s3_bucket.state.id
-  description = "The name of the state bucket"
-}
-
-output "bucket_arn" {
-  value       = aws_s3_bucket.state.arn
-  description = "The ARN of the state bucket"
-}
